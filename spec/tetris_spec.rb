c0165dc9ae38672079513d46f739323908c7f29b
liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-08-04 14:37:37 +1000

    class type checking test working

--- spec/tetris_spec.rb
+++ spec/tetris_spec.rb
@@ -2,9 +2,8 @@ require 'rspec'
 require_relative '../hw6assignment'
 
 describe MyTetris do
-    it "should have a rotation index" do
+    it "should be a Tetris" do
         t = MyTetris.new
-        expect(t.root).to be_a(TkRoot)
+        expect(t).is_a? Tetris
     end
 end
-
