d991941c1d1ec818931c42577390189b26325f94
liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-08-04 14:50:40 +1000

    test for keypress

--- spec/tetris_spec.rb
+++ spec/tetris_spec.rb
@@ -6,4 +6,12 @@ describe MyTetris do
         t = MyTetris.new
         expect(t).is_a? Tetris
     end
+    describe "key_bindings" do
+        context "when 'u' is pressed" do
+        it "should have a key binding for 'u'" do
+            t = MyTetris.new
+            expect(t).to respond_to(:key_bindings)
+        end
+    end
+end
 end
