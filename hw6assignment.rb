a66009a321786cabc0a952ba3befa0788c9167af
liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-08-05 10:58:42 +1000

    problem two done

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -2,14 +2,47 @@ require_relative './hw6provided'
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = All_Pieces + [[[[0, 0], [-1, 0], [1, 0], [2, 0], [-2, 0]], # long (only needs two)
+                                  [[0, 0], [0, -1], [0, 1], [0, 2], [0, -2]]],
+                                rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), # square with tail
+                                rotations([[0, 0], [1, 0], [0, 1]])] # short (only needs two)
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 end
 
+
 class MyBoard < Board
-  # your enhancements here
+  def initialize (game)
+    super(game)
+    @current_block = MyPiece.next_piece(self)
+  end
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..3).each{|index|
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] =
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
   def key_bindings
     super
     @root.bind('u', proc {@board.rotate_clockwise; @board.rotate_clockwise}) # rotate 180s
